21be1b15580dee737af30227c07099091f1d242c
manuelechavarria <maechavarriaor@gmail.com>
2026-03-28 17:05:15 -0500

    titulo AD

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -17,10 +17,10 @@ export default {
   },
   data: () => ({
     cuestionario: {
-      tema: 'Microcontroladores y sensores',
+      tema: 'Explorando emociones y habilidades comunicativas',
       titulo: 'Cuestionario',
       introduccion:
-        '<b> Objetivo:</b> Evaluar la comprensión y el uso adecuado de las herramientas básicas en la ventana <em>Board</em> del <em>software</em> EAGLE.',
+        '<b> Objetivo:</b> comprobar la apropiación de los conceptos fundamentales del componente formativo mediante la resolución de preguntas de selección múltiple sobre desarrollo personal, inteligencia emocional, comunicación y relaciones interpersonales.',
       barajarPreguntas: true,
       titulo_aprobado: '¡BUEN TRABAJO!',
       titulo_reprobado: 'VUELVA A INTENTARLO.',
